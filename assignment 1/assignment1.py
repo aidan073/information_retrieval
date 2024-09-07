5a64af585688f161e4479d5a5710f6fd48dad2be
aidan073 <aidanbell073@gmail.com>
2024-09-07 00:16:10 -0400

    Added new seperate file for creating indexes, repurposed assignment1.py

--- assignment 1/assignment1.py
+++ assignment 1/assignment1.py
@@ -1,14 +1,7 @@
-import pandas as pd
 import pyterrier as pt
-import json
 import os
 
-with open('Answers.json', 'r', encoding='utf-8') as file:
-    json_data = json.load(file)
-df = pd.DataFrame(json_data)
-
-index_location = os.path.join(os.getcwd(), 'index')
-os.makedirs(index_location, exist_ok=True)
-
-indexer = pt.DFIndexer(index_location, overwrite=True)
-indexer.index(df[['Id', 'Text']])
+index = pt.IndexFactory.of(os.path.join(os.getcwd(), "index"))
+retriever = pt.BatchRetrieve(index)
+results = retriever.search("math is")
+print(results.head())
---
+++ assignment 1/indexer.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import pyterrier as pt
+import shutil
+import json
+import os
+
+def make_path(folder_to_create: str):
+    """Create new folder in cwd
+
+    Args:
+        folder_to_create (str): name of folder to create in cwd
+
+    Returns:
+        index_location (str): file path of new folder
+    """
+    folder_path = os.path.join(os.getcwd(), folder_to_create)
+
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+
+    os.makedirs(folder_path)
+    return folder_path
+
+def json_to_df(path):
+    """Convert json file to pandas dataframe, formatted for pyterrier
+    
+    Args:
+        path (str): path to json file
+
+    Returns:
+        df (dataframe): pandas dataframe
+    """
+    with open('Answers.json', 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+    df = pd.DataFrame(json_data)
+    df = df.rename(columns={'Id': 'docno', 'Text': 'text'}) # pyterrier required format
+    return df
+
+def df_to_indexes(df):
+    """Convert df to indexes
+
+    Args:
+        df (dataframe): pandas dataframe
+
+    Returns:
+        index_ref: reference to indexes
+    """
+    index_location = make_path('index')
+    indexer = pt.IterDictIndexer(index_location)
+    index_ref = indexer.index(df.to_dict(orient="records"))
+    return index_ref
+
+df = json_to_df('Answers.json')
+index_ref = df_to_indexes(df)
