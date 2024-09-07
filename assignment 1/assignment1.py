b81cb45e0d07c2b73e828859b8366d1380d3dc88
aidan073 <aidanbell073@gmail.com>
2024-09-07 00:48:12 -0400

    begun retrieval creation in assignment1.py

--- assignment 1/assignment1.py
+++ assignment 1/assignment1.py
@@ -1,7 +1,22 @@
 import pyterrier as pt
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 import os
+
+stop_words = set(stopwords.words('english'))
 
 index = pt.IndexFactory.of(os.path.join(os.getcwd(), "index"))
 retriever = pt.BatchRetrieve(index)
-results = retriever.search("math is")
+
+def format_query(query):
+    tokens = word_tokenize(query)
+    filtered_query = ''
+    for token in tokens:
+        if not token.lower() in stop_words:
+            filtered_query += ' '+token.lower()
+    
+    return filtered_query
+
+query = format_query("Hello this is the best query")
+results = retriever.search(query)
 print(results.head())
