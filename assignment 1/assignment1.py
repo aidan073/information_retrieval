a923b2a4c343a2bbfcd38aaef13a5db3696b74da
aidan073 <aidanbell073@gmail.com>
2024-09-20 16:26:37 -0400

    tweaked search function ranking

--- assignment 1/assignment1.py
+++ assignment 1/assignment1.py
@@ -1,22 +1,27 @@
+import indexer
+import json
+import csv
+from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-import indexer
 
 stop_words = set(stopwords.words('english'))
 
-def format_query(query):
-    """Format query for searching
+def search_topics(in_file, out_file):
+    indexer1 = indexer.Indexer('Index.json')
+    with open(in_file, 'r', encoding='utf-8') as f:
+        topics = json.load(f)
+        with open(out_file, 'a', newline='') as f2:
+            writer = csv.writer(f2, delimiter='\t')
+            for topic in topics:
+                query = indexer.Indexer.process_text(topic['Body'])
+                result = indexer1.search(query)
+                rank = 1
+                for key, val in result.items():
+                    row = [topic['Id'], 'Q0', key, rank, val, 'BOW_Search']
+                    writer.writerow(row)
+                    rank+=1
+                    if rank > 100:
+                        break
 
-    Args:
-        query (str): free-text input query
-
-    Returns:
-        filtered_query (str): query prepared for searching
-    """
-    tokens = word_tokenize(query)
-    filtered_query = [token.lower() for token in tokens if token.isalpha() and token.lower() not in stop_words]
-    return filtered_query
-
-query = format_query("Hello, this is the best query!")
-indexer1 = indexer.Indexer('Index.json')
-res = indexer1.search(query)
+search_topics('topics_1.json', 'result_binary_3.tsv')
--- assignment 1/indexer.py
+++ assignment 1/indexer.py
@@ -10,18 +10,19 @@ class Indexer:
     def __init__(self, file_path):
         with open(file_path, 'r', encoding='utf-8') as f:
             self.documents = json.load(f)
-
-    def process_text(self, text):
+    
+    @staticmethod
+    def process_text(text):
         soup = BeautifulSoup(text, 'lxml')
         text = soup.get_text()
         tokens = word_tokenize(text)
         tokens = [token.lower() for token in tokens if token.isalpha() and token.lower() not in stop_words]
-        bow = Counter(tokens)
-        return bow
+        return tokens
     
     def index_docs(self):
         for document in self.documents:
-            bow = self.process_text(document.get('Text'))
+            tokens = Indexer.process_text(document.get('Text'))
+            bow = Counter(tokens)
             document['Text'] = bow
     
     def get_doc(self, doc_id):
@@ -30,10 +31,13 @@ class Indexer:
     def search(self, query):
         results = {}
         for document in self.documents:
-            for term in query:
-                if term in document.get('Text'):
-                    results[document['Id']] = results.get(document['Id'], 0) + document['Text'][term]
-        return sorted(results, key=results.get, reverse=True)
+            results[document['Id']] = 0
+            if len(document['Text']) > 5: # only consider documents with greater than 5 unique tokens (may need to be higher)
+                for term in query:
+                    if term in document.get('Text'):
+                        results[document['Id']] = results[document['Id']] + document['Text'][term]
+                results[document['Id']] = results[document['Id']]/sum(document['Text'].values()) # score = relevant terms / total terms
+        return dict(sorted(results.items(), key=lambda item: item[1], reverse=True))
     
     def save_index(self, outfile_path):
         with open(outfile_path, 'w') as f:
