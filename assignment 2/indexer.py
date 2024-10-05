be7d4446655b77a742d5a1ba5d36d1e2362d381a
aidan073 <aidanbell073@gmail.com>
2024-10-05 11:33:57 -0400

    added tfidf vectorization for each document

--- assignment 2/indexer.py
+++ assignment 2/indexer.py
@@ -1,6 +1,7 @@
 import json
 import pickle
 import math
+import numpy as np
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -29,17 +30,28 @@ def collect_vocab(doc_text, vocab, df_dict):
         doc_tf[token] = doc_tf.get(token, 0) + 1
     return doc_tf
 
-def getTFIDF(tf_dict, df_dict, total_docs):
-    total_terms = tf_dict.values().sum()
-    tfidf_dict = {term: tf_dict[term]/ total_terms * (math.log(total_docs / df_dict[term]) + 1) for term in tf_dict.keys()}
+def getTFIDF(tf_dict, df_dict, total_docs, token_count):
+    tfidf_dict = {term: (1 + math.log(tf_dict[term])) / token_count * (math.log(total_docs / df_dict[term]) + 1) for term in tf_dict.keys()}
     return tfidf_dict
 
+def getVectors(vocab, doc_tfidf):
+    doc_vecs = {}
+    index = 0
+    for term in vocab:
+        for doc_id, tfidf_dict in doc_tfidf:
+            doc_vecs.setdefault(doc_id, np.zeros(len(vocab)))
+            if term in tfidf_dict:
+                doc_vecs[doc_id][index] = tfidf_dict[term]         
+        index += 1
+                    
 def index(docs):
     vocab = []
     df_dict = {}
     doc_tfidf = {}
     for doc in docs:
         text = process_text(doc['Text'])
+        if len(text) == 0:
+            raise Exception(f"Empty body in document id: {doc['Id']}")
         tf_dict = collect_vocab(text, vocab, df_dict)
         doc_tfidf[doc['Id']] = tf_dict
     for doc_id, tf_dict in doc_tfidf.items():
