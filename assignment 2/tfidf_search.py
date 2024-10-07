ee176c5a669f2ab8671c80f21e8de5b544b33e68
aidan073 <aidanbell073@gmail.com>
2024-10-07 19:21:01 -0400

    added several search functions

--- assignment 2/tfidf_search.py
+++ assignment 2/tfidf_search.py
@@ -1,6 +1,7 @@
 import pickle
 import indexer
-import math
+import os
+import csv
 import numpy as np
 from collections import Counter
 
@@ -8,6 +9,9 @@ def getObjects(filepath):
     with open(filepath, 'rb') as f:
         objects = pickle.load(f)
     return objects
+
+def getQueries(filepath):
+    pass
 
 def queryVecs(query, vocab, df_dict, doc_bm25):
     query_tfidf = np.zeros(len(vocab))
@@ -23,12 +27,44 @@ def queryVecs(query, vocab, df_dict, doc_bm25):
         index+=1
     return query_tfidf, query_bm25
 
-def searchVecs():
-    pass
+def compareVecs(q_tfidf_vec, q_bm25_vec, d_tfidf_vec, d_bm25_vec):
+    tfidf_cos_sim = np.dot(q_tfidf_vec, d_tfidf_vec)/(np.norm(q_tfidf_vec)*np.norm(d_tfidf_vec))
+    bm25_cos_sim = np.dot(q_bm25_vec, d_bm25_vec)/(np.norm(q_bm25_vec)*np.norm(d_bm25_vec))
+    return tfidf_cos_sim, bm25_cos_sim
 
-query = 'This query should return a vector'
+def search(queries, vocab, df_dict, doc_bm25, d_tfidf_vec, d_bm25_vec, outfile_path_list):
+    override_check = 1
+    for query in queries:
+        id = query['Id']
+        text = query['Title'] + query['Body']
+        text = indexer.process_text(text)
+        query_tfidf, query_bm25 = queryVecs(text, vocab, df_dict, doc_bm25)
+        results_tfidf = {}
+        results_bm25 = {}
+        for doc_id in doc_bm25:
+            results_tfidf[doc_id], results_bm25[doc_id] = compareVecs(query_tfidf, query_bm25, d_tfidf_vec, d_bm25_vec)
+        results_tfidf = dict(sorted(results_tfidf.items(), key=lambda item: item[1], reverse=True))
+        results_bm25 = dict(sorted(results_bm25.items(), key=lambda item: item[1], reverse=True))
+        writeResult(results_tfidf, id, outfile_path_list[0], override_check)
+        writeResult(results_bm25, id, outfile_path_list[1])
+        override_check = 0
+
+def writeResult(result_dict, id, outfile_path, override_check):
+    if override_check and os.path.isfile(outfile_path):
+        os.remove(outfile_path)
+    with open(outfile_path, 'a', newline='') as f:
+        writer = csv.writer(f, delimiter='\t')
+        rank = 1
+        for doc_id, score in result_dict.items():
+            writer.writerow([id, 'Q0', doc_id, rank, score, 'vector_search'])
+            rank += 1
+            if rank >= 100:
+                break
+        
+
+
+queries = {}
 objects = getObjects('index.pkl')
-tokens = indexer.process_text(query)
-query_tfidf, query_bm25 = queryVecs(tokens, objects[0], objects[1], objects[2])
+search(queries, objects[0], objects[1], objects[2], objects[3], objects[4], ['result_tfidf_1.tsv', 'result_bm25_1.tsv'])
 pass
 #searchVecs()
