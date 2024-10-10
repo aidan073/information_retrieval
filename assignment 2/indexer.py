68943360cd8799c918109021647b815027583ee8
aidan073 <aidanbell073@gmail.com>
2024-10-09 22:51:15 -0400

    attempts

--- assignment 2/indexer.py
+++ assignment 2/indexer.py
@@ -2,13 +2,11 @@ import json
 import pickle
 import math
 import sys
-import random
 import numpy as np
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from scipy.sparse import dok_matrix, csr_matrix
-from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse import dok_matrix
 from nltk.stem import PorterStemmer
 
 stemmer = PorterStemmer()
@@ -67,16 +65,21 @@ def getVectors(vocab, doc_tfidf, doc_bm25):
     num_docs = len(doc_ids)
     vocab_size = len(vocab)
 
-    doc_vecs_tfidf = np.zeros((num_docs, vocab_size), dtype=np.float32)
-    doc_vecs_bm25 = np.zeros((num_docs, vocab_size), dtype=np.float32)
+    # sparse matrices instead of vectors for memory optimization
+    doc_vecs_tfidf = dok_matrix((num_docs, vocab_size), dtype=np.float32)
+    doc_vecs_bm25 = dok_matrix((num_docs, vocab_size), dtype=np.float32)
 
-    for doc_index, doc_id in enumerate(doc_ids):
-        for term_index, term in enumerate(vocab):
+    doc_id_to_index = {doc_id: idx for idx, doc_id in enumerate(doc_ids)} # map doc_id to indices
+
+    for term_index, term in enumerate(vocab):
+        for doc_id in doc_ids:
             if term in doc_tfidf[doc_id]:
-                doc_vecs_tfidf[doc_index, term_index] = doc_tfidf[doc_id][term]
-                doc_vecs_bm25[doc_index, term_index] = doc_bm25[doc_id][term]
-
-    return doc_vecs_tfidf, doc_vecs_bm25
+                doc_vecs_tfidf[doc_id_to_index[doc_id], term_index] = doc_tfidf[doc_id][term]
+                doc_vecs_bm25[doc_id_to_index[doc_id], term_index] = doc_bm25[doc_id][term]
+        #print(f"{term_index}/{vocab_size}")
+    doc_vecs_bm25 = doc_vecs_bm25.tocsr()
+    doc_vecs_tfidf = doc_vecs_tfidf.tocsr()
+    return doc_vecs_tfidf, doc_vecs_bm25, doc_id_to_index
                     
 def index(docs, outfile_path):
     vocab = []
@@ -90,27 +93,27 @@ def index(docs, outfile_path):
             pass
             #raise Exception(f"Empty body in document id: {doc['Id']}")
         doc_lengths.append(len(text))
-        doc_tfidf[doc['Id']] = getTF(text, 3)
+        doc_tfidf[doc['Id']] = getTF(text, 5)
     df_dict = getDF(doc_tfidf)
     avg_doc_length = sum(doc_lengths) / len(doc_lengths)
     doc_num = 0
     for doc_id, tf_dict in doc_tfidf.items():
         token_count = sum(tf_dict.values())
-        df_dict = filterDict(df_dict, 7)
+        df_dict = filterDict(df_dict, 15)
         doc_tfidf[doc_id] = getTFIDF(tf_dict, df_dict, len(docs), token_count)
         doc_bm25[doc_id] = getBM25(tf_dict, df_dict, doc_lengths[doc_num], avg_doc_length, len(docs), token_count)
         doc_num += 1
     vocab = list(df_dict.keys())
-    doc_vecs_tfidf, doc_vecs_bm25 = getVectors(vocab, doc_tfidf, doc_bm25)
-    save_objects(vocab, df_dict, doc_bm25, doc_vecs_tfidf, doc_vecs_bm25, outfile_path)
+    doc_vecs_tfidf, doc_vecs_bm25, doc_id_to_index = getVectors(vocab, doc_tfidf, doc_bm25)
+    save_objects(vocab, df_dict, doc_bm25, doc_vecs_tfidf, doc_vecs_bm25, doc_id_to_index, outfile_path)
 
-def save_objects(vocab, df_dict, doc_bm25, doc_vecs_tfidf, doc_vecs_bm25, outfile_path):
-    objects = [vocab, df_dict, doc_bm25, doc_vecs_tfidf, doc_vecs_bm25]
+def save_objects(vocab, df_dict, doc_bm25, doc_vecs_tfidf, doc_vecs_bm25, doc_id_to_index, outfile_path):
+    objects = [vocab, df_dict, doc_bm25, doc_vecs_tfidf, doc_vecs_bm25, doc_id_to_index]
     with open(outfile_path, 'wb') as f:
         pickle.dump(objects, f)
 
 if __name__ == "__main__":
-    sys.argv = ['indexer.py', 'test.json', 'index.pkl']
+    sys.argv = ['indexer.py', 'Answers.json', 'index.pkl']
     if len(sys.argv) <= 1:
         raise Exception(f"Missing {2-(len(sys.argv)-1)} required argument(s), refer to README")
     docs = readJSON(sys.argv[1])
--- assignment 2/search.py
+++ assignment 2/search.py
@@ -6,9 +6,9 @@ import csv
 import sys
 import heapq
 import numpy as np
-from numpy.linalg import norm
 from collections import Counter
 from scipy.sparse import dok_matrix, csr_matrix
+from sklearn.metrics.pairwise import cosine_similarity
 
 def getQueries(filepath):
     with open(filepath, 'r', encoding='utf-8') as f:
@@ -21,30 +21,29 @@ def getObjects(filepath):
     return objects
 
 def queryVecs(query, vocab, df_dict, doc_bm25):
-    
+
     if len(query) == 0:
         raise Exception("Empty query")
     
     vocab_size = len(vocab)
     word_counts = dict(Counter(query))
-    
-    # Create dense numpy arrays initialized with zeros
-    q_tfidf_vec = np.zeros((1, vocab_size), dtype=np.float32)
-    q_bm25_vec = np.zeros((1, vocab_size), dtype=np.float32)
+    q_tfidf_vec = dok_matrix((1, vocab_size), dtype=np.float32)
+    q_bm25_vec = dok_matrix((1, vocab_size), dtype=np.float32)
 
     for term_index, term in enumerate(vocab):
-        if term in word_counts:
+        if term in query:
             q_tfidf_vec[0, term_index] = indexer.termTFIDF(term, word_counts, df_dict, len(doc_bm25), len(query))
             q_bm25_vec[0, term_index] = indexer.termBM25(term, word_counts, df_dict, 1, 1, len(doc_bm25), len(query))
-    
+    q_tfidf_vec = q_tfidf_vec.tocsr()
+    q_bm25_vec = q_bm25_vec.tocsr()
     return q_tfidf_vec, q_bm25_vec
 
-def compareVecs(q_tfidf_vec, q_bm25_vec, d_tfidf_vec, d_bm25_vec):
-    tfidf_cos_sim = np.dot(q_tfidf_vec, d_tfidf_vec)/(norm(q_tfidf_vec)*norm(d_tfidf_vec))
-    bm25_cos_sim = np.dot(q_bm25_vec, d_bm25_vec)/(norm(q_bm25_vec)*norm(d_bm25_vec))
-    return tfidf_cos_sim, bm25_cos_sim
+def compute_cosine_similarity(q_tfidf_vec, q_bm25_vec, d_tfidf_vec, d_bm25_vec):
+    cos_sim_t = cosine_similarity(q_tfidf_vec, d_tfidf_vec)[0][0]
+    cos_sim_b = cosine_similarity(q_bm25_vec, d_bm25_vec)[0][0]
+    return cos_sim_t, cos_sim_b
 
-def search(queries, vocab, df_dict, doc_bm25, d_tfidf_vecs, d_bm25_vecs, outfile_path_list):
+def search(queries, vocab, df_dict, doc_bm25, d_tfidf_vecs, d_bm25_vecs, doc_id_to_index, outfile_path_list):
 
     if os.path.isfile(outfile_path_list[0]):
         os.remove(outfile_path_list[0])
@@ -62,10 +61,8 @@ def search(queries, vocab, df_dict, doc_bm25, d_tfidf_vecs, d_bm25_vecs, outfile
             query_tfidf_vec, query_bm25_vec = queryVecs(text, vocab, df_dict, doc_bm25)
             results_tfidf = {}
             results_bm25 = {}
-            for idx, doc_id in enumerate(doc_bm25):
-                results_tfidf[doc_id], results_bm25[doc_id] = compareVecs(query_tfidf_vec, query_bm25_vec, d_tfidf_vecs[idx], d_bm25_vecs[idx])
-            # results_tfidf = dict(sorted(results_tfidf.items(), key=lambda item: item[1], reverse=True))
-            # results_bm25 = dict(sorted(results_bm25.items(), key=lambda item: item[1], reverse=True))
+            for doc_id in doc_bm25:
+                results_tfidf[doc_id], results_bm25[doc_id] = compute_cosine_similarity(query_tfidf_vec, query_bm25_vec, d_tfidf_vecs[doc_id_to_index[doc_id]], d_bm25_vecs[doc_id_to_index[doc_id]])
             top_tfidf = heapq.nlargest(100, results_tfidf.items(), key=lambda item: item[1])
             top_bm25 = heapq.nlargest(100, results_bm25.items(), key=lambda item: item[1])
             writeResult(top_tfidf, id, f)
@@ -89,4 +86,5 @@ if len(sys.argv) <= 1:
     raise Exception(f"Missing {2-(len(sys.argv)-1)} required argument(s), refer to README")
 queries = getQueries(sys.argv[1])
 objects = getObjects(sys.argv[2])
-search(queries, objects[0], objects[1], objects[2], objects[3], objects[4], ['result_tfidf_1.tsv', 'result_bm25_1.tsv'])
+print(len(objects[0]))
+search(queries, objects[0], objects[1], objects[2], objects[3], objects[4], objects[5], ['result_tfidf_1.tsv', 'result_bm25_1.tsv'])
