b325be4c0fd302451911ea3377474b51a743ee48
Aidan Bell <aidanbell073@gmail.com>
2024-10-10 00:19:49 -0400

    Update search.py
    remove sparse, add batching

--- assignment 2/search.py
+++ assignment 2/search.py
@@ -7,8 +7,6 @@ import sys
 import heapq
 import numpy as np
 from collections import Counter
-from scipy.sparse import dok_matrix, csr_matrix
-from sklearn.metrics.pairwise import cosine_similarity
 
 def getQueries(filepath):
     with open(filepath, 'r', encoding='utf-8') as f:
@@ -21,30 +19,37 @@ def getObjects(filepath):
     return objects
 
 def queryVecs(query, vocab, df_dict, doc_bm25):
-
     if len(query) == 0:
         raise Exception("Empty query")
     
     vocab_size = len(vocab)
     word_counts = dict(Counter(query))
-    q_tfidf_vec = dok_matrix((1, vocab_size), dtype=np.float32)
-    q_bm25_vec = dok_matrix((1, vocab_size), dtype=np.float32)
+
+    q_tfidf_vec = np.zeros((1, vocab_size), dtype=np.float32)
+    q_bm25_vec = np.zeros((1, vocab_size), dtype=np.float32)
 
     for term_index, term in enumerate(vocab):
         if term in query:
             q_tfidf_vec[0, term_index] = indexer.termTFIDF(term, word_counts, df_dict, len(doc_bm25), len(query))
             q_bm25_vec[0, term_index] = indexer.termBM25(term, word_counts, df_dict, 1, 1, len(doc_bm25), len(query))
-    q_tfidf_vec = q_tfidf_vec.tocsr()
-    q_bm25_vec = q_bm25_vec.tocsr()
+    
     return q_tfidf_vec, q_bm25_vec
 
-def compute_cosine_similarity(q_tfidf_vec, q_bm25_vec, d_tfidf_vec, d_bm25_vec):
-    cos_sim_t = cosine_similarity(q_tfidf_vec, d_tfidf_vec)[0][0]
-    cos_sim_b = cosine_similarity(q_bm25_vec, d_bm25_vec)[0][0]
-    return cos_sim_t, cos_sim_b
+def batch_cosine_similarity(query_vectors, doc_vectors):
+    query_norms = np.linalg.norm(query_vectors, axis=1, keepdims=True)
+    doc_norms = np.linalg.norm(doc_vectors, axis=1, keepdims=True)
+
+    query_norms[query_norms == 0] = 1
+    doc_norms[doc_norms == 0] = 1
+
+    normalized_queries = query_vectors / query_norms
+    normalized_docs = doc_vectors / doc_norms
+
+    cosine_similarities = np.dot(normalized_queries, normalized_docs.T)
+
+    return cosine_similarities
 
 def search(queries, vocab, df_dict, doc_bm25, d_tfidf_vecs, d_bm25_vecs, doc_id_to_index, outfile_path_list):
-
     if os.path.isfile(outfile_path_list[0]):
         os.remove(outfile_path_list[0])
     if os.path.isfile(outfile_path_list[1]):
@@ -59,16 +64,21 @@ def search(queries, vocab, df_dict, doc_bm25, d_tfidf_vecs, d_bm25_vecs, doc_id_
             text = query['Title'] + query['Body']
             text = indexer.process_text(text)
             query_tfidf_vec, query_bm25_vec = queryVecs(text, vocab, df_dict, doc_bm25)
-            results_tfidf = {}
-            results_bm25 = {}
-            for doc_id in doc_bm25:
-                results_tfidf[doc_id], results_bm25[doc_id] = compute_cosine_similarity(query_tfidf_vec, query_bm25_vec, d_tfidf_vecs[doc_id_to_index[doc_id]], d_bm25_vecs[doc_id_to_index[doc_id]])
-            top_tfidf = heapq.nlargest(100, results_tfidf.items(), key=lambda item: item[1])
-            top_bm25 = heapq.nlargest(100, results_bm25.items(), key=lambda item: item[1])
+
+            results_tfidf = batch_cosine_similarity(query_tfidf_vec, d_tfidf_vecs)
+            results_bm25 = batch_cosine_similarity(query_bm25_vec, d_bm25_vecs)
+
+            results_tfidf_dict = {doc_id: results_tfidf[0, doc_id_to_index[doc_id]] for doc_id in doc_bm25}
+            results_bm25_dict = {doc_id: results_bm25[0, doc_id_to_index[doc_id]] for doc_id in doc_bm25}
+
+            top_tfidf = heapq.nlargest(100, results_tfidf_dict.items(), key=lambda item: item[1])
+            top_bm25 = heapq.nlargest(100, results_bm25_dict.items(), key=lambda item: item[1])
+
             writeResult(top_tfidf, id, f)
             writeResult(top_bm25, id, f2)
-            query_count+=1
-            print(f"{query_count}/{len(queries)}")
+
+            query_count += 1
+            print(f"queries processed: {query_count}/{len(queries)}")
     finally:
         f.close()
         f2.close()
